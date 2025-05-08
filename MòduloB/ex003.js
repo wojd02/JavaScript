e5f657e8ad9cca336119a51d2d3369aae58b0226
wojd02 <jonatanbayere@gmail.com>
2025-05-08 15:36:52 -0300

    final curso att

---
+++ MòduloB/ex003.js
@@ -0,0 +1,4 @@
+var n1 = Number.parseInt(window.prompt('Digite o primeiro número: '))
+var n2 = Number.parseInt(window.prompt('Digite o segundo número: '))
+var s = n1 + n2
+window.alert(`A soma entre ${n1} + ${n2} é igual a ${s}`) //${} - placeholder
